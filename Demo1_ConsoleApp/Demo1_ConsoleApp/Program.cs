49b3af1af1e6e05851938fb46a25f13bbc4fc304
Bruno Lopes <brunomlopes@gmail.com>
2012-05-05 17:26:19 +0100

    added classes, created embedded

--- Demo1_ConsoleApp/Demo1_ConsoleApp/Program.cs
+++ Demo1_ConsoleApp/Demo1_ConsoleApp/Program.cs
@@ -1,14 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Raven.Client.Embedded;
 
 namespace Demo1_ConsoleApp
 {
     class Program
     {
+        class Apresentação
+        {
+            public string Id { get; set; }
+            public string Nome { get; set; }
+            public DateTime Data { get; set; }
+            public TimeSpan Duração { get; set; }
+            public Pessoa Apresentador { get; set; }
+        }
+
+        class Pessoa
+        {
+            public string Nome { get; set; }
+        }
         static void Main(string[] args)
         {
+            var documentStore = new EmbeddableDocumentStore();
+            string id = "Apresentação RavenDB ISEL";
+
+            using(var session = documentStore.OpenSession())
+            {
+                var apresentação = new Apresentação()
+                                       {
+                                           Id = id,
+                                           Nome = "Document Databases com RavenDB",
+                                           Apresentador = new Pessoa()
+                                                              {
+                                                                  Nome = "Bruno Lopes"
+                                                              },
+                                           Data = new DateTime(2012, 05, 06, 09, 00, 00),
+                                           Duração = TimeSpan.FromHours(3),
+                                       };
+
+                session.Store(apresentação);
+                session.SaveChanges();
+            }
+            using(var session = documentStore.OpenSession())
+            {
+                var apresentação = session.Load<Apresentação>(id);
+
+                Console.Out.WriteLine("apresentação.Id = {0}", apresentação.Id);
+                Console.Out.WriteLine("apresentação.Nome = {0}", apresentação.Nome);
+                Console.Out.WriteLine("apresentação.Apresentador.Nome = {0}", apresentação.Apresentador.Nome);
+                Console.Out.WriteLine("apresentação.Data = {0}", apresentação.Data);
+                Console.Out.WriteLine("apresentação.Duração = {0}", apresentação.Duração);
+            }
         }
     }
 }
