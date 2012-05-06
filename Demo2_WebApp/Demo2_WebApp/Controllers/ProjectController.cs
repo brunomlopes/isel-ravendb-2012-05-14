ef28368d81cc4ec3686ba637909c9b517dd3e377
Bruno Lopes <brunomlopes@gmail.com>
2012-05-06 22:58:13 +0100

    Simple project controler with details

---
+++ Demo2_WebApp/Demo2_WebApp/Controllers/ProjectController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Demo2_WebApp.Infrastructure;
+using Demo2_WebApp.Models;
+
+namespace Demo2_WebApp.Controllers
+{
+    public class ProjectController : RavenDbController
+    {
+        public ActionResult Details(int id)
+        {
+            var project = RavenSession.Load<Project>(id);
+            return View(project);
+        }
+    }
+}
