e491ee0af2b256e75322e35bb06cb3597d6136e1
Bruno Lopes <brunomlopes@gmail.com>
2012-05-06 17:28:52 +0100

    Created model

---
+++ Demo2_WebApp/Demo2_WebApp/Models/Project.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Demo2_WebApp.Models
+{
+    public class Project
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public IList<Activity> Activities { get; set; }
+    }
+
+    public class Activity
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Person Owner { get; set; }
+        public IList<Task> Tasks { get; set; }
+    }
+
+    public class Task
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Person Owner { get; set; }
+    }
+
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
