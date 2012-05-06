4ddb5e0a44d248da3f0931fee40bc30c2c4e1014
Bruno Lopes <brunomlopes@gmail.com>
2012-05-06 22:58:13 +0100

    Simple project controler with details

---
+++ Demo2_WebApp/Demo2_WebApp/Infrastructure/RavenDbMVCExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo2_WebApp.Infrastructure
+{
+    public static class RavenDbMVCExtensions
+    {
+         public static int IntFromId(this string id)
+         {
+             return int.Parse(id.Split('/')[1]);
+         }
+    }
+}
