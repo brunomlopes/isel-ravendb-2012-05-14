46c17b39a8f1c326574ceb3a59e28c546a1da38c
Bruno Lopes <brunomlopes@gmail.com>
2012-05-06 23:11:31 +0100

    List

--- Demo2_WebApp/Demo2_WebApp/Controllers/ProjectController.cs
+++ Demo2_WebApp/Demo2_WebApp/Controllers/ProjectController.cs
@@ -1,11 +1,18 @@
 ﻿using System.Web.Mvc;
 using Demo2_WebApp.Infrastructure;
 using Demo2_WebApp.Models;
+using System.Linq;
 
 namespace Demo2_WebApp.Controllers
 {
     public class ProjectController : RavenDbController
     {
+        public ActionResult Index()
+        {
+            var projects = RavenSession.Query<Project>().ToList();
+            return View(projects);
+        }
+
         public ActionResult Details(int id)
         {
             var project = RavenSession.Load<Project>(id);
