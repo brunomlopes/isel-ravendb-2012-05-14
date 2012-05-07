f01c5bf0de19850ab1ab08ee4de440dabe962860
Bruno Lopes <brunomlopes@gmail.com>
2012-05-07 23:58:20 +0100

    Index with paging.

--- Demo2_WebApp/Demo2_WebApp/Controllers/ProjectController.cs
+++ Demo2_WebApp/Demo2_WebApp/Controllers/ProjectController.cs
@@ -2,15 +2,29 @@
 using Demo2_WebApp.Infrastructure;
 using Demo2_WebApp.Models;
 using System.Linq;
+using Demo2_WebApp.Models.ViewModels;
+using Raven.Client.Linq;
 
 namespace Demo2_WebApp.Controllers
 {
     public class ProjectController : RavenDbController
     {
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            var projects = RavenSession.Query<Project>().ToList();
-            return View(projects);
+            var viewModel = new ProjectIndexViewModel();
+
+            RavenQueryStatistics stats;
+            var projects = RavenSession.Query<Project>()
+                .Statistics(out stats)
+                .Skip((page-1) * 10)
+                .Take(10)
+                .ToList();
+
+            viewModel.TotalResults = stats.TotalResults;
+            
+            viewModel.Projects = projects;
+            viewModel.Page = page;
+            return View(viewModel);
         }
 
         public ActionResult Details(int id)
---
+++ Demo2_WebApp/Demo2_WebApp/Models/ViewModels/ProjectIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Web.Http.ValueProviders;
+
+namespace Demo2_WebApp.Models.ViewModels
+{
+    public class ProjectIndexViewModel
+    {
+        public IEnumerable<Project> Projects { get; set; }
+        public int TotalResults { get; set; }
+        public int Page { get; set; }
+    }
+}
