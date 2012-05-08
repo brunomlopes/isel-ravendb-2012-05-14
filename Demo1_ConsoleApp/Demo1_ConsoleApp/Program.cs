28f0a1998c183d75c6aeb20032b31a61c5297537
Bruno Lopes <brunomlopes>
2012-05-08 17:27:37 +0100

    Create Store, save apresentacao

--- Demo1_ConsoleApp/Demo1_ConsoleApp/Program.cs
+++ Demo1_ConsoleApp/Demo1_ConsoleApp/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Raven.Client;
+using Raven.Client.Embedded;
 
 namespace Demo1_ConsoleApp
 {
@@ -34,6 +36,24 @@ namespace Demo1_ConsoleApp
                 Date = new DateTime(2012, 05, 06, 09, 00, 00),
                 Duration = TimeSpan.FromHours(3),
             };
+            IDocumentStore documentStore = new EmbeddableDocumentStore();
+            documentStore.Initialize();
+
+            using (IDocumentSession session = documentStore.OpenSession())
+            {
+                session.Store(apresentação);
+                session.SaveChanges();
+            }
+            using (IDocumentSession session = documentStore.OpenSession())
+            {
+                Presentation presentation = session.Load<Presentation>("RavenDB/ISEL");
+
+                Console.Out.WriteLine("Presentation.Id = {0}", presentation.Id);
+                Console.Out.WriteLine("Presentation.Name = {0}", presentation.Name);
+                Console.Out.WriteLine("Presentation.Presenter.Name = {0}", presentation.Presenter.Nome);
+                Console.Out.WriteLine("Presentation.Date = {0}", presentation.Date);
+                Console.Out.WriteLine("Presentation.Duration = {0}", presentation.Duration);
+            }
         }
     }
 }
