2f53cb25c673c3ed8770def706c9062d7d6cb483
Bruno Lopes <brunomlopes>
2012-05-08 16:55:03 +0100

    Added classes

--- Demo1_ConsoleApp/Demo1_ConsoleApp/Program.cs
+++ Demo1_ConsoleApp/Demo1_ConsoleApp/Program.cs
@@ -7,6 +7,20 @@ namespace Demo1_ConsoleApp
 {
     class Program
     {
+        class Apresentação
+        {
+            public string Id { get; set; }
+            public string Nome { get; set; }
+            public DateTime Data { get; set; }
+            public TimeSpan Duração { get; set; }
+            public Pessoa Apresentador { get; set; }
+        }
+
+        class Pessoa
+        {
+            public string Nome { get; set; }
+        }
+
         static void Main(string[] args)
         {
         }
